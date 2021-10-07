2e3eb04a07fed2a1b743ad03d76fa5da3da44bff
SMolletSchool <shmollet@cpp.edu>
2021-10-07 16:59:55 -0700

    Finished user-side, with input validation and the works

--- loShuMagicSquare.c
+++ loShuMagicSquare.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int inputTest() {
-
+int inputTest(int givenInt, int givenArray[]) {
+    for (int l = 0; l < sizeof(givenArray)/sizeof(int); l++) { //Size of array will be 9 in this case, made function more generalized anyways
+        if (givenArray[l] == 0) { //If it's 0, then no need to check the rest of the array
+            givenArray[l] = givenInt; //Set to the value for next time in the function
+            return 1; //boolean True
+        }
+        else { //Value was found in the array
+            if (givenArray[l] == givenInt) return 0; //boolean False
+        }
+    }
 }
 
 int main() {
@@ -19,12 +28,12 @@ int main() {
         for (int j = 0; j < 3; j++) { //column value
             printf("input value from 1 to 9 (that hasn't already been inputted) into Lo Shu Square, space %d, %d: ", j, i);
             do {
-                scanf("%d", &userInput);
-                if (inputTest(userInput) == 0) userInput = 0; //If test fails, set userInput to invalid number
-                if (userInput > 0 && userInput < 10) testForLoShu[j][i] = userInput;
+                scanf("%d", &userInput); //Get user input, save in according variable
+                if (inputTest(userInput, previousInputs) == 0) userInput = 0; //If test fails, set userInput to invalid number
+                if (userInput > 0 && userInput < 10) testForLoShu[j][i] = userInput; //If test succeeds, set value in array to row j, column i
                 else printf("Invalid input! Input must be from 1 to 9, and cannot be repeating.");
             }
-            while (userInput < 1 || userInput > 9 && inputTest() == 1);
+            while (userInput < 1 || userInput > 9);
             
             
             //scanf("%d", &testForLoShu[j][i]);
